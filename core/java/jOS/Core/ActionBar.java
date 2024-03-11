a4ba35937ff2c026e16347281707515d4d1c07fe
_______166 <retromac9x2@gmail.com>
2024-03-11 11:23:21 +0000

    stuff

--- core/java/jOS/Core/ActionBar.java
+++ core/java/jOS/Core/ActionBar.java
@@ -3,6 +3,7 @@ package jOS.Core;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,17 @@ import androidx.appcompat.widget.Toolbar;
 
 public class ActionBar {
 
-    public static void actionBarConfig(int name, boolean home, AppCompatActivity context, String Action){
+    public static void actionBarConfig(int name, int logo, boolean home, AppCompatActivity context, String Action){
         Toolbar actionbar = (Toolbar) context.findViewById(R.id.toolbar);
         TextView Title = (TextView)context.findViewById(R.id.Title);
         ImageButton Back = (ImageButton)context.findViewById(R.id.back);
         ImageButton Menu = (ImageButton)context.findViewById(R.id.menu);
+        ImageView icon = (ImageView)context.findViewById(R.id.icon);
         context.setSupportActionBar(actionbar);
         context.getSupportActionBar().setDisplayShowTitleEnabled(false);
         context.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
         Title.setText(name);
+        icon.setImageResource(logo);
         Back.setImageResource(R.drawable.ic_ab_back_holo_dark_am);
         if (!home) {
             Back.setVisibility(View.VISIBLE);
