c1cef368f111683f456fa3deecab341b3d62227a
_______166 <retromac9x2@gmail.com>
2024-04-06 21:54:08 +0100

    fix crashes with legacy actionbar

--- core/j-SDK-core/src/main/java/jOS/Core/ActionBar.java
+++ core/j-SDK-core/src/main/java/jOS/Core/ActionBar.java
@@ -12,7 +12,7 @@ import androidx.appcompat.widget.Toolbar;
 
 public class ActionBar {
 
-    public static void actionBarConfig(String name, int logo, boolean home, AppCompatActivity context, String Action){
+    public static void actionBarConfig(String name, int logo, boolean home, AppCompatActivity context){
         Toolbar actionbar = (Toolbar) context.findViewById(R.id.toolbar);
         TextView Title = (TextView)context.findViewById(R.id.Title);
         ImageButton Back = (ImageButton)context.findViewById(R.id.back);
@@ -21,24 +21,19 @@ public class ActionBar {
         context.setSupportActionBar(actionbar);
         if (context.getSupportActionBar() == null) {
             Log.i("ActionBar", "You forgot to import the action bar in the layout file");
-        }
-        context.getSupportActionBar().setDisplayShowTitleEnabled(false);
-        context.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-        Title.setText(name);
-        icon.setImageResource(logo);
-        Back.setImageResource(R.drawable.j_ic_ab_back_holo_dark_am);
-        if (!home) {
-            Back.setVisibility(View.VISIBLE);
-            Back_content.setClickable(true);
-        } else if (home) {
-            Back.setVisibility(View.GONE);
-            Back_content.setClickable(false);
-        }
-        Back_content.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                context.onBackPressed();
+        } else {
+            context.getSupportActionBar().setDisplayShowTitleEnabled(false);
+            context.getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            Title.setText(name);
+            icon.setImageResource(logo);
+            Back.setImageResource(R.drawable.j_ic_ab_back_holo_dark_am);
+            if (!home) {
+                Back.setVisibility(View.VISIBLE);
+                Back_content.setClickable(true);
+            } else if (home) {
+                Back.setVisibility(View.GONE);
+                Back_content.setClickable(false);
             }
-        });
+        }
     }
 }
