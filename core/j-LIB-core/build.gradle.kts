cbd789d6485e0f3c17485f67994b67b56ef6f525
._______166 <retromac9x2@gmail.com>
2024-09-05 11:52:35 +0100

    fix it for good

--- core/j-LIB-core/build.gradle.kts
+++ core/j-LIB-core/build.gradle.kts
@@ -1,9 +1,9 @@
 import com.vanniktech.maven.publish.SonatypeHost
 import org.gradle.api.provider.Provider
 
-val Ver: String = providers.exec {
-    commandLine("cat", "ver")
-}.standardOutput.asText.get().trim()
+val Ver: String = "1"//providers.exec {
+    //commandLine("cat", "ver")
+//}.standardOutput.asText.get().trim()
 
 
 
@@ -60,6 +60,7 @@ dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     api("androidx.compose.ui:ui-android:1.7.0")
     api("androidx.compose.material3:material3-android:1.3.0")
+    api("androidx.compose.material:material-android:1.7.0")
     api("io.coil-kt:coil-compose:2.7.0")
     api("com.google.accompanist:accompanist-drawablepainter:0.36.0")
     api("com.google.accompanist:accompanist-placeholder-material:0.36.0")
