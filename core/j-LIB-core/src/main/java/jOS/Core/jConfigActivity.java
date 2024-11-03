596038079e2a0df088a225983596e355bc526d31
._______166 <retromac9x2@gmail.com>
2024-11-03 14:46:03 +0000

    fix config activity (i hope)

--- core/j-LIB-core/src/main/java/jOS/Core/jConfigActivity.java
+++ core/j-LIB-core/src/main/java/jOS/Core/jConfigActivity.java
@@ -1,6 +1,5 @@
 package jOS.Core;
 
-import static androidx.preference.PreferenceFragmentCompat.ARG_PREFERENCE_ROOT;
 import static jOS.Core.ThemeEngine.ThemeEngine.currentTheme;
 import static jOS.Core.ThemeEngine.ThemeEngine.getThemeFromDB1;
 
@@ -11,15 +10,11 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.WindowCompat;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartFragmentCallback;
-import androidx.preference.PreferenceFragmentCompat.OnPreferenceStartScreenCallback;
 import androidx.preference.PreferenceScreen;
 
 import java.util.Objects;
@@ -29,18 +24,11 @@ import jOS.Core.utils.ErrorUtils;
 /**
  * Settings activity for Launcher. Currently implements the following setting: Allow rotation
  */
-public class jConfigActivity extends jActivity
-        implements OnPreferenceStartFragmentCallback, OnPreferenceStartScreenCallback {
+public class jConfigActivity extends jActivity {
 
-    public int preferenceFragmentValue() {
-        return R.string.settings_fragment_name;
+    public jLIBSettingsFragment preferenceFragment() {
+        return new jLIBSettingsFragment();
     }
-
-    public static final String EXTRA_FRAGMENT_ARGS = ":settings:fragment_args";
-
-    // Intent extra to indicate the pref-key of the root screen when opening the settings activity
-    public static final String EXTRA_FRAGMENT_ROOT_KEY = ARG_PREFERENCE_ROOT;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,64 +37,17 @@ public class jConfigActivity extends jActivity
 
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
-        Intent intent = getIntent();
-
         if (savedInstanceState == null) {
-            Bundle args = intent.getBundleExtra(EXTRA_FRAGMENT_ARGS);
-            if (args == null) {
-                args = new Bundle();
-            }
-
-            String root = intent.getStringExtra(EXTRA_FRAGMENT_ROOT_KEY);
-            if (!TextUtils.isEmpty(root)) {
-                args.putString(EXTRA_FRAGMENT_ROOT_KEY, root);
-            }
-
-            final FragmentManager fm = getSupportFragmentManager();
-            final Fragment f = fm.getFragmentFactory().instantiate(getClassLoader(),
-                    getString(preferenceFragmentValue()));
-            f.setArguments(args);
             // Display the fragment as the main content.
-            fm.beginTransaction().replace(R.id.content_frame, f).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, preferenceFragment()).commit();
         }
-    }
-
-    private boolean startPreference(String fragment, Bundle args, String key) {
-        if (getSupportFragmentManager().isStateSaved()) {
-            // Sometimes onClick can come after onPause because of being posted on the handler.
-            // Skip starting new preferences in that case.
-            return false;
-        }
-        final FragmentManager fm = getSupportFragmentManager();
-        final Fragment f = fm.getFragmentFactory().instantiate(getClassLoader(), fragment);
-        if (f instanceof DialogFragment) {
-            f.setArguments(args);
-            ((DialogFragment) f).show(fm, key);
-        } else {
-            startActivity(new Intent(this, this.getClass())
-                    .putExtra(EXTRA_FRAGMENT_ARGS, args));
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onPreferenceStartFragment(
-            PreferenceFragmentCompat preferenceFragment, Preference pref) {
-        return startPreference(pref.getFragment(), pref.getExtras(), pref.getKey());
-    }
-
-    @Override
-    public boolean onPreferenceStartScreen(PreferenceFragmentCompat caller, PreferenceScreen pref) {
-        Bundle args = new Bundle();
-        args.putString(ARG_PREFERENCE_ROOT, pref.getKey());
-        return startPreference(getString(preferenceFragmentValue()), args, pref.getKey());
     }
 
 
     /**
-     * This fragment shows the launcher preferences.
+     * This fragment shows the preferences.
      */
-    public static class LauncherSettingsFragment extends PreferenceFragmentCompat {
+    public static class jLIBSettingsFragment extends PreferenceFragmentCompat {
 
         public boolean isLIBConfig() {
             return false;
@@ -120,10 +61,6 @@ public class jConfigActivity extends jActivity
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            initPreference(rootKey);
-        }
-
-        public void initPreference(String rootKey){
             setPreferencesFromResource(preferenceXML(), rootKey);
             if (!hideLIB()) {
                 addPreferencesFromResource(R.xml.lib_preference);
@@ -172,6 +109,13 @@ public class jConfigActivity extends jActivity
                 case "LIBVer":
                     Log.i("Preference Logging", "LIBVer Found!!!!");
                     preference.setSummary(BuildConfig.LIBVersion);
+                    preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                        @Override
+                        public boolean onPreferenceClick(@NonNull Preference preference) {
+                            startActivity(new Intent(preference.getContext(), LIBAboutActivity.class));
+                            return true;
+                        }
+                    });
                     return true;
             }
             return extraPrefs(preference);
