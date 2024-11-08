031866383b3cdb986f2c12f7e763c4c96ba18585
._______166 <retromac9x2@gmail.com>
2024-11-08 20:22:23 +0000

    [TEMP] import material-components 1.12.0
    is here until AOSP updates their dependencies

--- aosp-libs/app/build.gradle.kts
+++ aosp-libs/app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("com.google.accompanist:accompanist-drawablepainter:0.36.0")
     implementation("com.mikepenz:aboutlibraries-core:11.2.3")
     implementation("com.mikepenz:aboutlibraries-compose-m3:11.2.3")
+    implementation("com.google.android.material:material:1.12.0")// TEMPORARY, is imported until AOSP updates their version of material components to 1.12.0 or newer
 }
 
 
@@ -63,6 +64,7 @@ configure<GenerateBpPluginExtension> {
         when {
             module.group == "androidx.databinding" -> false
             module.group == "com.google.accompanist" -> false
+            module.group == "com.google.android.material" -> false // TEMPORARY, is set to false until AOSP updates their version of material components to 1.12.0 or newer
             module.group.startsWith("androidx") -> true
             module.group.startsWith("com.google") -> true
             else -> false
