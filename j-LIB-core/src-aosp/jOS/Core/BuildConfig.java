5dd167cb9dd05e42259d1d0e1e88662a1286a600
._______166 <retromac9x2@gmail.com>
2024-11-08 20:31:51 +0000

    copy generated build config
    should fix last compiler error

---
+++ j-LIB-core/src-aosp/jOS/Core/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package jOS.Core;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "jOS.Core";
+  public static final String BUILD_TYPE = "release";
+  // Field from default config.
+  public static final String LIBVersion = "3.3.1";
+}
