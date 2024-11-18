b7b303c35337c2cae5d0a43c91c4b6d79a5c5a14
._______166 <62702353+dot166@users.noreply.github.com>
2024-11-18 16:22:48 +0000

    Update MenuButton.java

--- j-LIB-core/src/main/java/jOS/Core/MenuButton.java
+++ j-LIB-core/src/main/java/jOS/Core/MenuButton.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
@@ -30,7 +31,7 @@ public class MenuButton extends LinearLayout {
             Drawable icon = styledAttributes.getDrawable(R.styleable.MenuButton_android_icon);
             int iconTint = styledAttributes.getColor(R.styleable.MenuButton_tint, context.getTheme().obtainStyledAttributes(new int[]{android.R.attr.textColorPrimary}).getColor(0, getResources().getColor(R.color.j_primary_text_holo_dark)));
 
-            if (icon != null && iconTint != null) {
+            if (icon != null && icon instanceof VectorDrawable) {
                 icon.setTint(iconTint);
             }
             setIcon(icon);
