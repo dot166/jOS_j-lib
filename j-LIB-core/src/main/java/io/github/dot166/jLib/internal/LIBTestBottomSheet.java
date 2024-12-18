d3c1c96f6948932247f2f2cc6de4365a17160d3f
._______166 <retromac9x2@gmail.com>
2024-12-18 10:44:08 +0000

    improve birdgame and remove unused resources

--- j-LIB-core/src/main/java/io/github/dot166/jLib/internal/LIBTestBottomSheet.java
+++ j-LIB-core/src/main/java/io/github/dot166/jLib/internal/LIBTestBottomSheet.java
@@ -1,6 +1,8 @@
 package io.github.dot166.jLib.internal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,7 @@ import androidx.annotation.RestrictTo;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
+import io.github.dot166.jLib.BirdGameActivity;
 import io.github.dot166.jLib.R;
 
 @RestrictTo(RestrictTo.Scope.LIBRARY)
@@ -23,6 +26,13 @@ public class LIBTestBottomSheet extends BottomSheetDialogFragment {
             public void onClick(View v) {
                 Toast.makeText(v.getContext(), "Exiting...", Toast.LENGTH_SHORT).show();
                 requireActivity().finish();
+            }
+        });
+        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Starting BirdGame");
+                startActivity(new Intent(v.getContext(), BirdGameActivity.class));
             }
         });
         return view;
