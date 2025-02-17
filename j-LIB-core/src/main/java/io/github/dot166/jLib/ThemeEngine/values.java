8b9ee40fefa1bda04c9583aceb176b781c28120a
._______166 <62702353+dot166@users.noreply.github.com>
2025-02-17 18:12:37 +0000

    remove deprecated functions

--- j-LIB-core/src/main/java/io/github/dot166/jLib/ThemeEngine/values.java
+++ j-LIB-core/src/main/java/io/github/dot166/jLib/ThemeEngine/values.java
@@ -2,7 +2,6 @@ package io.github.dot166.jLib.ThemeEngine;
 
 import android.content.Context;
 
-import androidx.compose.material3.ColorScheme;
 import androidx.compose.material3.Shapes;
 import androidx.compose.material3.Typography;
 
@@ -10,31 +9,7 @@ public interface values {
 
     public int jLibTheme();
 
-    /**
-     * @Deprecated M3 Theme is now DayNight
-     */
-    @Deprecated(since = "4.0.1", forRemoval = true)
-    public int M3L();
-
     public int M3();
-
-    /**
-     * @Deprecated M3 Theme is now DayNight
-     */
-    @Deprecated(since = "4.0.1", forRemoval = true)
-    public int M3D();
-
-    /**
-     * @Deprecated Compose Theme now uses attributes from the selected theme resource
-     */
-    @Deprecated(since = "4.0.1", forRemoval = true)
-    public ColorScheme DComposeColourScheme(Context context);
-
-    /**
-     * @Deprecated Compose Theme now uses attributes from the selected theme resource
-     */
-    @Deprecated(since = "4.0.1", forRemoval = true)
-    public ColorScheme LComposeColourScheme(Context context);
 
     public Typography ComposeTypography(Context context);
 
