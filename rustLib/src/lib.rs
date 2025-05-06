a081715a017b86f04f96ea6d14bd3a7130cbf5d7
._______166 <retromac9x2@gmail.com>
2025-05-06 13:13:00 +0100

    add binary and decimal converters

--- rustLib/src/lib.rs
+++ rustLib/src/lib.rs
@@ -12,5 +12,7 @@ mod tests {
     fn it_works() {
         assert_eq!(is_even(2), true);
         assert_eq!(is_odd(1), true);
+        assert_eq!(dec_to_bin(8), "1000");
+        assert_eq!(bin_to_dec("1000".parse().unwrap()), 8);
     }
 }
--- rustLib/src/maths.rs
+++ rustLib/src/maths.rs
@@ -1,7 +1,24 @@
-pub fn is_even(i: i64) -> bool {
+pub fn is_even(i: u32) -> bool {
     i % 2 == 0
 }
 
-pub fn is_odd(i: i64) -> bool {
+pub fn is_odd(i: u32) -> bool {
     !is_even(i)
+}
+
+pub fn dec_to_bin(mut decimal: u32) -> String {
+    let mut binary_string = String::new();
+    if decimal == 0 {
+        return "0".to_string();
+    }
+    while decimal > 0 {
+        let remainder = decimal % 2;
+        binary_string.insert(0, char::from_digit(remainder, 10).unwrap());
+        decimal /= 2;
+    }
+    binary_string
+}
+
+pub fn bin_to_dec(binary_string: String) -> u32 {
+    u32::from_str_radix(&*binary_string, 2).unwrap()
 }
