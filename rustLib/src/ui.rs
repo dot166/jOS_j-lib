34d1e1f6a62f9191ff060dc7b0ca461b02d44689
._______166 <retromac9x2@gmail.com>
2025-05-07 09:50:29 +0100

    add console input dialogs

--- rustLib/src/ui.rs
+++ rustLib/src/ui.rs
@@ -1,3 +1,4 @@
+use std::io::*;
 use gtk4 as gtk;
 use gtk::prelude::*;
 use gtk::{glib, Application, ApplicationWindow};
@@ -24,4 +25,26 @@ pub fn supports_gui() -> bool {
     });
 
     app.run() == glib::ExitCode::SUCCESS
+}
+
+pub fn console_yn_dialog(message: &str) -> bool {
+    println!("{} [Y/N]", message);
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("error: unable to read user input");
+    input = input.trim().to_uppercase();
+    if input == "Y" || input == "YES" {
+        true
+    } else if input == "N" || input == "NO" { 
+        false
+    } else {
+        println!("invalid input");
+        console_yn_dialog(message)
+    }
+}
+
+pub fn console_ok_dialog(message: &str) {
+    println!("{}", message);
+    println!("press enter to continue");
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect(""); // no panic message required (i think)
 }
