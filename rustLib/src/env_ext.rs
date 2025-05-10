ed1cb443d25c69840a6ce3c40c76c222450ec2f0
._______166 <retromac9x2@gmail.com>
2025-05-10 18:08:45 +0100

    env_ext fixes

--- rustLib/src/env_ext.rs
+++ rustLib/src/env_ext.rs
@@ -1,12 +1,11 @@
 use std::env;
-use std::env::VarError;
 
-pub fn get_config_folder() -> Result<String, VarError> {
+pub fn get_config_folder() -> Result<String, env::VarError> {
     if env::consts::OS == "macos" || env::consts::OS == "linux" {
         env::var("XDG_CONFIG_HOME")
     } else if env::consts::OS == "windows" {
         env::var("APPDATA")
     } else { 
-        Err(VarError::NotPresent) // this only works on desktop platforms
+        Err(env::VarError::NotPresent) // this only works on desktop platforms
     }
 }
