c7029439e176e480628599a373d3ab5fb2773242
._______166 <retromac9x2@gmail.com>
2025-05-10 18:09:21 +0100

    add android build stuff

--- rustLib/src/env_ext.rs
+++ rustLib/src/env_ext.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process::Output;
 
 pub fn get_config_folder() -> Result<String, env::VarError> {
     if env::consts::OS == "macos" || env::consts::OS == "linux" {
@@ -7,5 +8,14 @@ pub fn get_config_folder() -> Result<String, env::VarError> {
         env::var("APPDATA")
     } else { 
         Err(env::VarError::NotPresent) // this only works on desktop platforms
+    }
+}
+
+pub fn parse_env_from_output(output: Output) {
+    let output_str = String::from_utf8(output.stdout).unwrap();
+    for line in output_str.lines() {
+        if let Some((var, value)) = line.split_once('=') {
+            unsafe { env::set_var(var, value); }
+        }
     }
 }
