205870b4a9944b0b7c4b949ff412f3c292c479b4
._______166 <retromac9x2@gmail.com>
2025-05-28 12:09:55 +0100

    add rss reading capability
    this took far to long

--- j-LIB-core/src/main/java/io/github/dot166/jlib/internal/LIBTestBottomSheet.java
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/internal/LIBTestBottomSheet.java
@@ -9,12 +9,14 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.RestrictTo;
+import androidx.preference.PreferenceManager;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import io.github.dot166.jlib.BirdGameActivity;
 import io.github.dot166.jlib.ObstacleGameActivity;
 import io.github.dot166.jlib.R;
+import io.github.dot166.jlib.rss.RSSActivity;
 
 @RestrictTo(RestrictTo.Scope.LIBRARY)
 public class LIBTestBottomSheet extends BottomSheetDialogFragment {
@@ -41,6 +43,13 @@ public class LIBTestBottomSheet extends BottomSheetDialogFragment {
             public void onClick(View v) {
                 Log.i(TAG, "Starting ObstacleGame");
                 startActivity(new Intent(v.getContext(), ObstacleGameActivity.class));
+            }
+        });
+        view.findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Starting RSSActivity");
+                startActivity(new Intent(v.getContext(), RSSActivity.class));
             }
         });
         return view;
---
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/rss/RSSNotifier.java
@@ -0,0 +1,94 @@
+package io.github.dot166.jlib.rss;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.preference.PreferenceManager;
+
+import com.prof18.rssparser.model.RssChannel;
+import com.prof18.rssparser.model.RssItem;
+
+import java.util.List;
+import java.util.Objects;
+
+import io.github.dot166.jlib.R;
+import io.github.dot166.jlib.app.Notifier;
+import io.github.dot166.jlib.web.jWebIntent;
+
+public class RSSNotifier extends Notifier {
+    private final Context context;
+    private final String notificationChannelId = "rss_channel_id";
+    private final String notificationChannelName = "RSS Feed Notifications";
+    private final int notificationId = 200;
+    private RssChannel rssChannel;
+
+    public RSSNotifier(NotificationManager notificationManager, Context context) {
+        super(notificationManager);
+        this.context = context;
+    }
+
+    @Override
+    public String getNotificationChannelId() {
+        return notificationChannelId;
+    }
+
+    @Override
+    public String getNotificationChannelName() {
+        return notificationChannelName;
+    }
+
+    @Override
+    public int getNotificationId() {
+        return notificationId;
+    }
+
+    @Override
+    public Notification buildNotification() {
+        jWebIntent webIntent = new jWebIntent(context);
+        webIntent.setUrl(rssChannel.getItems().get(0).getLink());
+        webIntent.configureWebView(true, true);
+        return new NotificationCompat.Builder(context, getNotificationChannelId())
+                .setContentTitle(getNotificationTitle())
+                .setContentText(getNotificationMessage())
+                .setSmallIcon(R.drawable.outline_rss_feed_24)
+                .setContentIntent(PendingIntent.getActivity(context, 0, webIntent.getIntent(), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE))
+                .build();
+    }
+
+    @Override
+    public String getNotificationTitle() {
+        return rssChannel.getTitle() + " - " + rssChannel.getItems().get(0).getTitle();
+    }
+
+    @Override
+    public String getNotificationMessage() {
+        return rssChannel.getItems().get(0).getDescription();
+    }
+
+    @Override
+    public void showNotification() {
+        String rssUrl = PreferenceManager.getDefaultSharedPreferences(context).getString("rssUrl", "");
+        Log.i("RSS", rssUrl);
+        rssChannel = (new RSSViewModel()).fetchFeedWithoutViewModel(rssUrl, context);
+        if (Objects.equals(rssChannel.getItems().get(0).getDescription(), "Something failed and to keep the app running this is displayed")) { // should only trigger on error handler
+            Log.e("RSS", "cannot display notification as RSS reader is in fallback mode");
+            return;
+        }
+        int rssCount = 0;
+        List<RssItem> articles = rssChannel.getItems();
+        rssCount = articles.size(); // should be safe to do this, there is no way on earth that anyone has Integer.MAX_VALUE rss items (2^31 - 1)
+        Log.i("RSS", String.valueOf(rssCount));
+
+        int savedRssCount = PreferenceManager.getDefaultSharedPreferences(context).getInt("savedRssCount", 0);
+        if (savedRssCount < rssCount) {
+            super.showNotification();
+            PreferenceManager.getDefaultSharedPreferences(context).edit().putInt("savedRssCount", rssCount).apply(); // use shared prefs to not annoy user, just hope nobody has a preference with savedRssCount as the key and uses the rss feature
+        }
+    }
+}
+
