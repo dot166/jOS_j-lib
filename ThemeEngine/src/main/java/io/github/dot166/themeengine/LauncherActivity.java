c168982f056aecc2404be72e1ea2d26256a5f4c7
._______166 <62702353+dot166@users.noreply.github.com>
2025-06-21 13:11:22 +0100

    remove useless invoke function
    just moved the code for it into the onCreate()

--- ThemeEngine/src/main/java/io/github/dot166/themeengine/LauncherActivity.java
+++ ThemeEngine/src/main/java/io/github/dot166/themeengine/LauncherActivity.java
@@ -8,16 +8,12 @@ public class LauncherActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        invokeSettings();
-        finish();
-    }
-
-    private void invokeSettings() {
         final Intent intent = new Intent();
         intent.setClass(this, ThemeEngineActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
                 | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+        finish();
     }
 
 }
