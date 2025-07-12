e16ce6a496a10e91d0d5883fe65b250de27fd709
._______166 <retromac9x2@gmail.com>
2025-07-13 00:08:31 +0100

    agp upgrade and add codename infra

--- j-LIB-core/src/main/java/io/github/dot166/jlib/jos/Build.java
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/jos/Build.java
@@ -1,5 +1,9 @@
 package io.github.dot166.jlib.jos;
 
+import static io.github.dot166.jlib.app.jLIBCoreApp.TAG;
+
+import android.util.Log;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -22,4 +26,49 @@ public class Build {
     public static final String jOS_RELEASE = getInternalRelease();
     /** A boolean value utilized to determine if the device is running jOS */
     public static final boolean is_jOS = !jOS_RELEASE.equals("0");
+
+    private static String getCodeNameInternal() {
+        String version = jOS_RELEASE.split("-")[0].replace("-", "").split("\\.")[0].replace(".", "");
+        switch (version) {
+            case "0" -> {
+                Log.e(TAG, "Device is not running jOS, returning '0'");
+                return "0";
+            }
+            case "15" -> {
+                return "Obsidian";
+            }
+            case "16" -> {
+                return "Plasma";
+            }
+            default -> {
+                Log.i(TAG, "this version of jLib is too old for this jOS version, quick return the jOS Version");
+                return version;
+            }
+        }
+    }
+
+    public static String jOS_CODENAME = getCodeNameInternal();
+
+    private static String getShortCodeNameInternal() {
+        if (isNumber(jOS_CODENAME)) {
+            // must have a number, just return it
+            return jOS_CODENAME;
+        } else {
+            return jOS_CODENAME.substring(0, 1);
+        }
+    }
+
+    private static boolean isNumber(String str) {
+        if (str.length() == 1) {
+            return str.matches("[0-9]");
+        } else if (str.length() == 2) {
+            return str.matches("[0-9][0-9]");
+        } else if (str.length() == 3) {
+            return str.matches("[0-9][0-9][0-9]");
+        } else {
+            return false; // not bothering for any larger numbers just yet
+        }
+    }
+
+    public static String jOS_CODENAME_SHORT = getShortCodeNameInternal();
 }
