4ff8aecd41d0df8816c6762d527e336a8d8d53bf
._______166 <retromac9x2@gmail.com>
2025-08-19 15:19:38 +0100

    get rid of gtk

--- rustLib/src/ui.rs
+++ rustLib/src/ui.rs
@@ -1,31 +1,4 @@
 use crate::io_ext::read_console_input;
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
-use gtk4 as gtk;
-
-pub fn supports_gui() -> bool {
-    let app = Application::builder()
-        .application_id("io.github.dot166.jlibrust.test-ui")
-        .build();
-
-    app.connect_activate(|app| {
-        // We create the main window.
-        let window = ApplicationWindow::builder()
-            .application(app)
-            .default_width(100)
-            .default_height(100)
-            .title("Test GUI Support")
-            .resizable(false)
-            .build();
-
-        // Show the window.
-        window.present();
-        // get status code immediately
-        app.quit();
-    });
-
-    app.run() == glib::ExitCode::SUCCESS
-}
 
 pub fn console_yn_dialog(message: &str) -> bool {
     println!("{} [Y/N]", message);
