537e2d5c860c85fbc8c279fde53c801ca99a78d3
._______166 <retromac9x2@gmail.com>
2025-08-29 17:36:17 +0100

    Revert "update dependencies"
    This reverts commit 8299834851abbe86bf8fe896a2692f670b3f61c1.

--- j-LIB-core/build.gradle.kts
+++ j-LIB-core/build.gradle.kts
@@ -71,12 +71,12 @@ dependencies {
     //noinspection KtxExtensionAvailable
     api("androidx.preference:preference:1.2.1")
     api("com.google.android.material:material:1.12.0")
-    api("androidx.core:core-ktx:1.17.0")
+    api("androidx.core:core-ktx:1.16.0")
     api("androidx.browser:browser:1.9.0")
     api("androidx.activity:activity-compose:1.10.1")
-    api("androidx.compose.ui:ui-android:1.9.0")
+    api("androidx.compose.ui:ui-android:1.8.3")
     api("androidx.compose.material3:material3-android:1.3.2")
-    api("androidx.compose.material:material-android:1.9.0")
+    api("androidx.compose.material:material-android:1.8.3")
     api("io.coil-kt:coil-compose:2.7.0")
     api("com.google.accompanist:accompanist-drawablepainter:0.37.3")
     api("com.mikepenz:aboutlibraries-core:12.2.4")
@@ -127,8 +127,6 @@ configure<GenerateBpPluginExtension> {
             module.group.startsWith("org.jetbrains") -> false
             module.group == "com.google.accompanist" -> false
             module.group == "androidx.media3" -> false // media3 is not mapped in generateBp because it is only visible to android automotive and some test things, jLib (and apps that depend an it) are (most likely) not any of these
-            module.name == "savedstate-compose-android" -> false // aosp does not have it
-	    module.name == "runtime-annotation-android" -> false // see above comment
 	    module.group.startsWith("androidx") -> true
             module.group.startsWith("com.google") -> true
             else -> false
