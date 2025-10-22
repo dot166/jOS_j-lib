4a08df24fba17a13a1d03f1a7c8c37abcb483921
._______166 <retromac9x2@gmail.com>
2025-10-22 15:12:53 +0100

    fix build.kt

--- j-LIB-core/src/main/java/io/github/dot166/jlib/jos/Build.kt
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/jos/Build.kt
@@ -13,7 +13,7 @@ object Build {
         get() {
             val date = Date(Build.TIME)
             val formattedDate =
-                SimpleDateFormat("yMMdd", Locale.UK).format(date) + "j"
+            SimpleDateFormat("yMMdd", Locale.UK).format(date) + "j"
             if (formattedDate == Build.DISPLAY) {
                 return VersionUtils.androidVersion + "-" + Build.DISPLAY.replace(
                     "j",
@@ -24,69 +24,73 @@ object Build {
         }
 
 
-    /** A String utilized to distinguish jOS versions  */
-    @JvmField
-    val jOS_RELEASE: String = internalRelease
+        /** A String utilized to distinguish jOS versions  */
+        @JvmField
+        val jOS_RELEASE: String = internalRelease
 
-    /** A boolean value utilized to determine if the device is running jOS  */
-    val is_jOS: Boolean = jOS_RELEASE != "0"
+        /** A boolean value utilized to determine if the device is running jOS  */
+        @JvmField
+        val is_jOS: Boolean = jOS_RELEASE != "0"
 
-    private val codeNameInternal: String
-        get() {
-            val version =
+        private val codeNameInternal: String
+            get() {
+                val version =
                 jOS_RELEASE.split("-".toRegex())
-                    .dropLastWhile { it.isEmpty() }.toTypedArray()[0].replace("-", "")
-                    .split("\\.".toRegex()).dropLastWhile { it.isEmpty() }
-                    .toTypedArray()[0].replace(".", "")
-            when (version) {
-                "0" -> {
-                    Log.e(jLIBCoreApp.TAG, "Device is not running jOS, returning '0'")
-                    return "0"
-                }
+                .dropLastWhile { it.isEmpty() }.toTypedArray()[0].replace("-", "")
+                .split("\\.".toRegex()).dropLastWhile { it.isEmpty() }
+                .toTypedArray()[0].replace(".", "")
+                when (version) {
+                    "0" -> {
+                        Log.e(jLIBCoreApp.TAG, "Device is not running jOS, returning '0'")
+                        return "0"
+                    }
 
-                "15" -> {
-                    return "Obsidian"
-                }
+                    "15" -> {
+                        return "Obsidian"
+                    }
 
-                "16" -> {
-                    return "Plasma"
-                }
+                    "16" -> {
+                        return "Plasma"
+                    }
 
-                else -> {
-                    Log.i(
-                        jLIBCoreApp.TAG,
-                        "this version of jLib is too old for this jOS version, quick return the jOS Version"
-                    )
-                    return version
+                    else -> {
+                        Log.i(
+                            jLIBCoreApp.TAG,
+                            "this version of jLib is too old for this jOS version, quick return the jOS Version"
+                        )
+                        return version
+                    }
                 }
             }
-        }
 
-    var jOS_CODENAME: String = codeNameInternal
+            @JvmField
+            var jOS_CODENAME: String = codeNameInternal
 
-    var jOS_RELEASE_WITH_CODENAME: String = "$jOS_RELEASE ($jOS_CODENAME)"
+            @JvmField
+            var jOS_RELEASE_WITH_CODENAME: String = "$jOS_RELEASE ($jOS_CODENAME)"
 
-    private val shortCodeNameInternal: String?
-        get() {
-            if (isNumber(jOS_CODENAME)) {
-                // must have a number, just return it
-                return jOS_CODENAME
-            } else {
-                return jOS_CODENAME.substring(0, 1)
-            }
-        }
+            private val shortCodeNameInternal: String?
+                get() {
+                    if (isNumber(jOS_CODENAME)) {
+                        // must have a number, just return it
+                        return jOS_CODENAME
+                    } else {
+                        return jOS_CODENAME.substring(0, 1)
+                    }
+                }
 
-    private fun isNumber(str: String): Boolean {
-        if (str.length == 1) {
-            return str.matches("[0-9]".toRegex())
-        } else if (str.length == 2) {
-            return str.matches("[0-9][0-9]".toRegex())
-        } else if (str.length == 3) {
-            return str.matches("[0-9][0-9][0-9]".toRegex())
-        } else {
-            return false // not bothering for any larger numbers just yet
-        }
-    }
+                private fun isNumber(str: String): Boolean {
+                    if (str.length == 1) {
+                        return str.matches("[0-9]".toRegex())
+                    } else if (str.length == 2) {
+                        return str.matches("[0-9][0-9]".toRegex())
+                    } else if (str.length == 3) {
+                        return str.matches("[0-9][0-9][0-9]".toRegex())
+                    } else {
+                        return false // not bothering for any larger numbers just yet
+                    }
+                }
 
-    var jOS_CODENAME_SHORT: String? = shortCodeNameInternal
+                @JvmField
+                var jOS_CODENAME_SHORT: String? = shortCodeNameInternal
 }
