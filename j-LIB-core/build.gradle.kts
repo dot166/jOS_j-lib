9e64214b724a6207dac7fcfb91531795f012b846
._______166 <retromac9x2@gmail.com>
2025-11-03 11:03:46 +0000

    update generateBp

--- j-LIB-core/build.gradle.kts
+++ j-LIB-core/build.gradle.kts
@@ -20,7 +20,7 @@ apply {
 
 buildscript {
     repositories {
-        maven("https://raw.githubusercontent.com/dot166/gradle-generatebp/v1.25.1/.m2")
+        maven("https://raw.githubusercontent.com/dot166/gradle-generatebp/v1.29.1/.m2")
     }
 
     dependencies {
