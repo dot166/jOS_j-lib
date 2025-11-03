5c69c2e53857098fd0cd7cacf873092f98052b7c
._______166 <retromac9x2@gmail.com>
2025-11-03 11:10:48 +0000

    remove unused dependencies and resources

--- j-LIB-core/src/main/java/io/github/dot166/jlib/app/OSSLicenceActivity.kt
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/app/OSSLicenceActivity.kt
@@ -6,8 +6,6 @@ import android.view.MenuItem
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -18,6 +16,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import com.mikepenz.aboutlibraries.ui.compose.android.produceLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
 import io.github.dot166.jlib.R
@@ -40,7 +39,7 @@ class OSSLicenceActivity : jActivity() {
                                         onBackPressedDispatcher.onBackPressed()
                                     }
                                 ) {
-                                    Icon(Icons.AutoMirrored.Filled.ArrowBack,
+                                    Icon(painterResource(id = R.drawable.arrow_back_24px),
                                         "backIcon"
                                     )
                                 }
