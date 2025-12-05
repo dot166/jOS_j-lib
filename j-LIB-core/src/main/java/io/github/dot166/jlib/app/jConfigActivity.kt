66cf86b39f486448fa4c33be437f7dc4acaf03c6
._______166 <retromac9x2@gmail.com>
2025-12-05 21:44:00 +0000

    fix preference with multiple fragments

--- j-LIB-core/src/main/java/io/github/dot166/jlib/app/jConfigActivity.kt
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/app/jConfigActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.annotation.RestrictTo
 import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
 import com.android.settingslib.preference.PreferenceFragment
 import io.github.dot166.jlib.R
@@ -12,7 +13,7 @@ import io.github.dot166.jlib.utils.VersionUtils.libVersion
 /**
  * jLib Settings activity.
  */
-open class jConfigActivity : CollapsingToolbarBaseActivity() {
+open class jConfigActivity : CollapsingToolbarBaseActivity(), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
     open fun preferenceFragment(): PreferenceFragment {
         return ExamplePrefFragment()
     }
@@ -38,8 +39,30 @@ open class jConfigActivity : CollapsingToolbarBaseActivity() {
             libPref.setIcon(R.mipmap.ic_launcher_j)
             libPref.setTitle(R.string.jlib_version)
             libPref.setSummary(libVersion)
+            libPref.fragment
             screen.addPreference(libPref)
             preferenceScreen = screen
         }
+    }
+
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat,
+        pref: Preference
+    ): Boolean {
+        val fragment = supportFragmentManager.fragmentFactory.instantiate(
+            classLoader,
+            pref.fragment!!
+        ).apply {
+            arguments = pref.extras
+            setTargetFragment(caller, 0)
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.content_frame, fragment)
+            .addToBackStack(null)
+            .commit()
+
+        title = pref.title
+        return true
     }
 }
