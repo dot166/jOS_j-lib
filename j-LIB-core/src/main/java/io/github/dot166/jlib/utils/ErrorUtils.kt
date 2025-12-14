3eeae882fd6cb0e54d133fca561426c471681a86
._______166 <retromac9x2@gmail.com>
2025-12-14 20:23:20 +0000

    rework theme

--- j-LIB-core/src/main/java/io/github/dot166/jlib/utils/ErrorUtils.kt
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/utils/ErrorUtils.kt
@@ -3,7 +3,9 @@ package io.github.dot166.jlib.utils
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import androidx.appcompat.app.AlertDialog
+import com.android.settingslib.widget.SettingsThemeHelper
+import com.android.settingslib.widget.theme.R as SLR
 import io.github.dot166.jlib.R
 
 object ErrorUtils {
@@ -20,35 +22,36 @@ object ErrorUtils {
     fun handle(e: Throwable, context: Context) {
         e.printStackTrace()
         try {
-            val builder = MaterialAlertDialogBuilder(context)
-
-            builder.setMessage(R.string.dialog_fail_message)
+            val theme = if (SettingsThemeHelper.isExpressiveTheme(context)) {
+                SLR.style.Theme_AlertDialog_SettingsLib_Expressive
+            } else {
+                SLR.style.Theme_AlertDialog_SettingsLib
+            }
+            AlertDialog.Builder(context, theme)
+                .setMessage(R.string.dialog_fail_message)
                 .setTitle(R.string.dialog_fail_title)
-                .setIcon(R.mipmap.icon_error)
+                .setIcon(context.packageManager.getApplicationIcon(context.packageName))
                 .setCancelable(false)
                 .setPositiveButton(
                     R.string.dialog_stacktrace
                 ) { dialog, id ->
-                    val builder2 = MaterialAlertDialogBuilder(context)
-
-                    builder2.setMessage(
+                    AlertDialog.Builder(context, theme)
+                        .setMessage(
                         e.toString() + "\n" + e.stackTrace.contentToString()
                             .replace(", ".toRegex(), "\n")
-                    )
+                        )
                         .setTitle(R.string.dialog_fail_title)
-                        .setIcon(R.mipmap.icon_error)
+                        .setIcon(context.packageManager.getApplicationIcon(context.packageName))
                         .setCancelable(false)
                         .setNeutralButton(
                             R.string.ok
                         ) { dialog, id -> dialog.dismiss() }
-                    //Creating dialog box
-                    builder2.show()
+                            .show()
                 }
-            builder.setNegativeButton(
-                R.string.dialog_ignore
-            ) { dialog, id -> Log.i(TAG, "IGNORING ERROR") }
-            //Creating dialog box
-            builder.show()
+                .setNegativeButton(
+                    R.string.dialog_ignore
+                ) { dialog, id -> Log.i(TAG, "IGNORING ERROR") }
+                .show()
         } catch (de1: Exception) {
             Log.e(TAG, "Error handler Broke!!")
             de1.printStackTrace()
