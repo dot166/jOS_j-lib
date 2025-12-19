c3e87725b5d9f1694448587ba616739607821233
._______166 <retromac9x2@gmail.com>
2025-12-19 12:42:32 +0000

    improve preference activity

--- j-LIB-core/src/main/java/io/github/dot166/jlib/app/jConfigActivity.kt
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/app/jConfigActivity.kt
@@ -2,13 +2,16 @@ package io.github.dot166.jlib.app
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
 import com.android.settingslib.preference.PreferenceFragment
 
 /**
  * jLib Settings activity.
  */
-abstract class jConfigActivity : CollapsingToolbarBaseActivity() {
+abstract class jConfigActivity : CollapsingToolbarBaseActivity(), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
     abstract fun preferenceFragment(): PreferenceFragment
 
     @SuppressLint("PrivateResource")
@@ -19,5 +22,19 @@ abstract class jConfigActivity : CollapsingToolbarBaseActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame, preferenceFragment()).addToBackStack(null).commit()
         }
+    }
+
+    override fun onPreferenceStartFragment(
+        caller: PreferenceFragmentCompat,
+        pref: Preference
+    ): Boolean {
+        val args: Bundle = pref.getExtras()
+        val fragment: Fragment = supportFragmentManager.getFragmentFactory().instantiate(
+            classLoader, pref.fragment!!
+        )
+        fragment.setArguments(args)
+        supportFragmentManager.beginTransaction()
+            .add(com.android.settingslib.collapsingtoolbar.R.id.content_frame, fragment).addToBackStack(null).commit()
+        return true
     }
 }
