5e75ab27acea5d1d153edbf082103181b446204a
._______166 <retromac9x2@gmail.com>
2025-12-19 12:42:56 +0000

    remove deprecated functions

--- j-LIB-core/src/main/java/io/github/dot166/jlib/app/jLIBCoreApp.kt
+++
@@ -1,6 +0,0 @@
-package io.github.dot166.jlib.app
-
-import android.app.Application
-
-@Deprecated("this does nothing now, please extend android.app.Application")
-open class jLIBCoreApp : Application()
