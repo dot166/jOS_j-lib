2a1a54fdd79682855affebff766cd02afff05464
._______166 <retromac9x2@gmail.com>
2026-05-20 12:30:52 +0100

    try and fix dialog

--- j-LIB-core/src/main/java/io/github/dot166/jlib/app/SettingsLibComposeDialog.kt
+++ j-LIB-core/src/main/java/io/github/dot166/jlib/app/SettingsLibComposeDialog.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Card
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.OutlinedButton
@@ -34,11 +35,13 @@ fun SettingsLibComposeDialog(
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .wrapContentHeight()
                     .padding(16.dp),
             ) {
                 Column(
                     modifier = Modifier
-                        .fillMaxSize(),
+                        .fillMaxSize()
+                        .wrapContentHeight() ,
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
